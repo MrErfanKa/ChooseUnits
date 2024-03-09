73441d39baa03ccc5df4bb4403d0b83a9bb07dd7
Erfan Ka <mr.erfan.man@gmail.com>
2024-03-09 12:59:58 +0330

    base form of Colleges

--- src/Base/University/College.java
+++ src/Base/University/College.java
@@ -1,4 +1,29 @@
 package Base.University;
 
+import Base.Units.Course;
+
+import java.util.ArrayList;
+
 public class College {
+    private final ArrayList<Course> courses;
+    //check final :
+    private final String name;
+    public College(String name){
+        courses = new ArrayList<>();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void AddCourseToCollege(Course course){
+        courses.add(course);
+    }
+    public void removeCourseFromCollege(Course course){
+        courses.remove(course);
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
 }
